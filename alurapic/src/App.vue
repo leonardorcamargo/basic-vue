6921853a1a4912ea0dc5649aaf2c0919480dd689
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-06-16 13:55:28 -0300

    Adicionado função animação no componente Painel.vue quando esconder imagem, utilizando <transition> do vue. Criado componente ImagemResponsiva.vue para separar a imagem do App.vue, apenas agrupando as responsabilidades de exibir imagens responsivas para um componente.

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -7,7 +7,7 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
         <meu-painel :titulo="foto.titulo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">          
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
         </meu-painel>
       
       </li>
@@ -17,12 +17,14 @@
 
 <script>
   import Painel from './components/shared/painel/Painel.vue';
+  import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';  
   
   export default {
     
     components: {
 
-      'meu-painel': Painel
+      'meu-painel': Painel,
+      'imagem-responsiva': ImagemResponsiva
     },
     
     data() {
@@ -74,10 +76,6 @@
 
   .lista-fotos .lista-fotos-item {
     display: inline-block;
-  }
-
-  .imagem-responsiva {
-    width: 100%;
   }
 
   .filtro {
