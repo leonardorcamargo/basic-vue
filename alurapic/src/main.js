4d14b4d149042b459f8fabaa2c2184b5ea3c042d
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-06-19 06:44:27 -0300

    Importando arquivo de rotas e criando uma instância dele em main.js

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
+import { routes } from './routes';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
+const router = new VueRouter({
+  /*routes: routes; Em ECS6, quando o valor da propriedade tem o mesmo nome, podemos deixar 
+    simplismente o nome da propriedade */
+  routes
+});
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
