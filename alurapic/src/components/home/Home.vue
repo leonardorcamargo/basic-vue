16cac4148004707b45f0be3e60fa5608d91f1224
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-06-19 23:20:12 -0300

    Adicionando evento para clique do botão no componente Home.vue

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -8,7 +8,7 @@
 
 				<meu-painel :titulo="foto.titulo">
 					<imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-					<meu-botao rotulo="remover" tipo="button"/>
+					<meu-botao rotulo="remover" tipo="button" @click.native="remove(foto)"/>
 				</meu-painel>			
 			</li>
 		</ul>
@@ -27,6 +27,16 @@
 			'meu-painel': Painel,
 			'imagem-responsiva': ImagemResponsiva,
 			'meu-botao': Botao
+		},
+
+		methods: {
+
+			remove(foto) {
+				if (confirm('Confirma?')) {
+				
+					alert (foto.titulo);
+				}
+			}
 		},
 		
 		data() {
