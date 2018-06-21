25827d9fcbb87a6615ab0fb1e6923da5875ff880
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-06-20 23:24:46 -0300

    Criação de diretiva v-meu-transform para rotacionar componentes com duplo clique. Ajustes no Home.vue para utilizar nova diretiva na imagem.  Changes to be committed: 	modified:   src/components/home/Home.vue 	new file:   src/directives/Transform.js 	modified:   src/main.js

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -7,7 +7,7 @@
 			<li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
 				<meu-painel :titulo="foto.titulo">
-					<imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+					<imagem-responsiva v-meu-transform :url="foto.url" :titulo="foto.titulo"/>
 					<meu-botao 
 						rotulo="remover" 
 						tipo="button" 
