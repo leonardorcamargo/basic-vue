01692a92ccdb62d6057b3cdc1798ac791307055d
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-06-20 23:42:52 -0300

    Ajustes na diretiva para possibilitar a passar de parâmetros.

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -7,7 +7,8 @@
 			<li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
 				<meu-painel :titulo="foto.titulo">
-					<imagem-responsiva v-meu-transform :url="foto.url" :titulo="foto.titulo"/>
+					<imagem-responsiva :url="foto.url" :titulo="foto.titulo" 
+						v-meu-transform="{rotate: 15,animate: true}"/>
 					<meu-botao 
 						rotulo="remover" 
 						tipo="button" 
--- alurapic/src/directives/Transform.js
+++ alurapic/src/directives/Transform.js
@@ -3,12 +3,23 @@ import Vue from 'vue';
 Vue.directive('meu-transform', {
 
 	bind(el, binding, vnode) {
+		let rotate = 90;
+		let animate = false;
+
+		if (binding) {
+			rotate = binding.value.rotate || 90;
+			animate = binding.value.animate || false;
+		}
 
 		let current = 0;
-		el.addEventListener('dblclick',() => {
+		el.addEventListener('dblclick',function() {
 
-			current += 90;
-			el.style.transform = `rotate(${current}deg)`;
+			current += rotate;
+			this.style.transform = `rotate(${current}deg)`;
+
+			if (animate){
+				this.style.transition = "transform 0.5s";
+			}
 		})
 	}
 });
