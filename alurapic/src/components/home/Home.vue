42cf69d14bf1180d845e4f90acb7d1b1b6c03535
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-06-21 06:27:21 -0300

    Adicinando argumentos na diretiva, possibilitando utilizar algum efeito específico nos elementos

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -8,7 +8,7 @@
 
 				<meu-painel :titulo="foto.titulo">
 					<imagem-responsiva :url="foto.url" :titulo="foto.titulo" 
-						v-meu-transform="{rotate: 15,animate: true}"/>
+						v-meu-transform:scale.animate="1.2"/>
 					<meu-botao 
 						rotulo="remover" 
 						tipo="button" 
--- alurapic/src/directives/Transform.js
+++ alurapic/src/directives/Transform.js
@@ -3,21 +3,30 @@ import Vue from 'vue';
 Vue.directive('meu-transform', {
 
 	bind(el, binding, vnode) {
-		let rotate = 90;
-		let animate = false;
 
-		if (binding) {
-			rotate = binding.value.rotate || 90;
-			animate = binding.value.animate || false;
-		}
+		let current = 0;	
 
-		let current = 0;
 		el.addEventListener('dblclick',function() {
 
-			current += rotate;
-			this.style.transform = `rotate(${current}deg)`;
+			let incremento = binding.value || 0;
+			let efeito;
 
-			if (animate){
+			if (!binding.arg || binding.arg == 'rotate') {
+
+				if (binding.modifiers.reverse) {
+					current -= incremento;
+				} else {
+					current += incremento;
+				}
+				efeito = `rotate(${current}deg)`;
+			} else if (binding.arg == 'scale') {
+
+				efeito = `scale(${incremento})`;
+			}
+
+			this.style.transform = efeito;
+
+			if (binding.modifiers.animate){
 				this.style.transition = "transform 0.5s";
 			}
 		})
