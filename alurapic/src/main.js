17d022e65f2dcdba2e69d7118bd403cc2c4597e0
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-07-07 12:53:56 -0300

    Importando modulo vee-validate ao projeto

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
+import VeeValidate from 'vee-validate';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
 Vue.use(VueRouter);
+Vue.use(VeeValidate);
 
 const router = new VueRouter({
   /*routes: routes; Em ECS6, quando o valor da propriedade tem o mesmo nome, podemos deixar 
