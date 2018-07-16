06b76ec29fce683f7ac3805dabbf2a0094bf13e6
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-07-16 10:09:51 -0300

    Foi criado um arquivo de teste CSS e importado no main.js para ser utilizado pela aplicação

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -7,6 +7,7 @@ import './directives/Transform';
 import VeeValidate from 'vee-validate';
 import msg from './pt_BR';
 import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/teste.css';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
