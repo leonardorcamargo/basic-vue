64299e72f360312343aca367d4faf5b88be46cb4
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-07-19 11:06:02 -0300

    Adicionando javascript do Bootstrap e resolvendo problema do jQuery. Adicionado um plugin no webpack.config.js para carregar as variaveis do jQuery antes de importar o Bootstrap.js

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -9,6 +9,8 @@ import msg from './pt_BR';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/teste.css';
 import './assets/js/teste.js';
+import 'bootstrap/dist/js/bootstrap.js';
+
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
