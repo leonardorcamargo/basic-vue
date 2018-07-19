a2418ce6e527e8ea8b5feb1886bd47fc8f6a826a
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-07-19 10:55:40 -0300

    Novo arquivo teste.js para ser utilizado para códigos de escopo global (não é recomendado, mas caso seja necessário) Importado no main.js para ser acessível em toda aplicação

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -8,6 +8,7 @@ import VeeValidate from 'vee-validate';
 import msg from './pt_BR';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/teste.css';
+import './assets/js/teste.js';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
